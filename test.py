1f7c4621db83d4fae094f4f851233033eec139fb
Vickytor <58131905+Vickytor@users.noreply.github.com>
2019-11-24 16:42:29 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -18,7 +18,8 @@ red.height = 162
 
 batch = pyglet.graphics.Batch()
 color_bubbles = [blue, yellow, red]
-direction = [-100, 100]
+direction_list = []
+bir = []
 bubble_list = []
 counter = 0
 counter_update = 0
@@ -27,13 +28,19 @@ def new_bubble(x, y):
 	bubble = pyglet.sprite.Sprite(random.choice(color_bubbles), batch=batch)
 	bubble.position = (x - 81, y - 81)
 	bubble_list.append(bubble)
+	dir = [-100,100]
+	direction_list.append(dir)
+
 	
 def update(dt):
-	for bubble in bubble_list:
-		bubble.x += 100*dt
-		bubble.y += 100*dt
-		
-class Bubbles: 
+	for i in range(len(bubble_list)):
+		bubble_list[i].x += direction_list[i][0]*dt
+		bubble_list[i].y += direction_list[i][1]*dt
+		if bubble_list[i].x == window.width - 162 or bubble_list[i].x == 0:
+			direction_list[i][0] *= -1
+		if bubble_list[i].y == window.height - 162 or bubble_list[i].y == 0:
+			direction_list[i][1] *= -1
+
 	
 
 	
